91eb453f464b60bde930056229d21dd78c340755
houdunwang <2300071698@qq.com>
2017-04-23 19:59:51 +0800

    no message

--- src/ValidateProvider.php
+++ src/ValidateProvider.php
@@ -7,21 +7,27 @@
  * |    WeChat: aihoudun
  * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
  * '-------------------------------------------------------------------*/
+
 namespace houdunwang\validate;
 
 use houdunwang\framework\build\Provider;
 
-class ValidateProvider extends Provider {
+class ValidateProvider extends Provider
+{
+    //延迟加载
+    public $defer = true;
 
-	//延迟加载
-	public $defer = true;
+    public function boot()
+    {
+    }
 
-	public function boot() {
-	}
-
-	public function register() {
-		$this->app->single( 'Validate', function () {
-			return new Validate();
-		} );
-	}
+    public function register()
+    {
+        $this->app->single(
+            'Validate',
+            function () {
+                return new Validate();
+            }
+        );
+    }
 }
